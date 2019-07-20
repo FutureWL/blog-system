5d5091a269128d1a84768ba204f86760db19d936
FutureWL <624263934@qq.com>
2019-07-20 13:57:42 +0800

    update

---
+++ blog-system-server/src/main/java/io/github/futurewl/BlogApplication.java
@@ -0,0 +1,12 @@
+package io.github.futurewl;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai
+ */
+@SpringBootApplication
+public class BlogApplication {
+}
